d25b94ecbbccdfe13f1d39715b4d514e4aa31098
Thomas Sertorio <thomas.sertorio@gmail.com>
2017-06-14 10:07:37 +0100

    Fine

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,25 @@
 class ProductsController < ApplicationController
   skip_before_action  :authenticate_user!
 
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   def index
     @products = Product.all
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :tagline)
   end
 end
